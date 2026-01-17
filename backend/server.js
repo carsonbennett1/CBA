82d9baea6f40787886e1c5f611e0946d62fe7383
Carson Bennett <carben2004@gmail.com>
2026-01-16 22:27:44 -0800

    db model setup pt2

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from './config/db.js';
+import { connectDB } from "./config/db.js";
+import Book from "./models/book.model.js";
 
 dotenv.config();
 
-const app = express()
+const app = express();
+app.use(express.json()); // allows acceptance of JSON data in req body
 
-app.post("/books", async (req, res) => {
+app.post("/api/books", async (req, res) => {
     const book = req.body; // user will send this data
     if(!book.name || !book.pages || !book.image){
         return res.status(400).json({ success:false, message: "Please provide all fields"});
     }
 
-    const newBook = new Book(book)
+    const newBook = new Book(book);
 
     try {
         await newBook.save();
-        res.status(201).json({ success: true, date: newBook })
+        res.status(201).json({ success: true, date: newBook });
     }catch (errpr){
         console.error("Error in created product:", error.message);
         res.status(500).json({success: false, message: "Server error "});
     }
-
 });
 
 app.listen(5000, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000")
+    console.log("Server started at http://localhost:5000");
 });
