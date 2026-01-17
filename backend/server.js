f259b036aa50a9ffa45257f9d60ecbf70f31ffbd
Carson Bennett <carben2004@gmail.com>
2026-01-17 10:22:32 -0800

    adding GET and DELETE api handling

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ dotenv.config();
 const app = express();
 app.use(express.json()); // allows acceptance of JSON data in req body
 
+app.get("/api/books", async (req, res) => {
+    try {
+        const books = await Book.find({});
+        res.status(200).json({ success: true, data: books });
+    } catch (error) {
+        console.log("error in fetching products:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
 app.post("/api/books", async (req, res) => {
     const book = req.body; // user will send this data
     if(!book.name || !book.pages || !book.image){
@@ -22,6 +32,16 @@ app.post("/api/books", async (req, res) => {
     }catch (errpr){
         console.error("Error in created product:", error.message);
         res.status(500).json({success: false, message: "Server error "});
+    }
+});
+
+app.delete("/api/books/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Book.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Book deleted"});
+    } catch (error) {
+        res.status(404).json({success: false, message: "Book not found"});
     }
 });
 
