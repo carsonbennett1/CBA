8cf04bb363bbd38b55896027fd0972e97ef28c64
Carson Bennett <carben2004@gmail.com>
2026-01-24 21:51:31 -0800

    adding submission boxes and methods for adding books

--- frontend/src/pages/AddBook.jsx
+++ frontend/src/pages/AddBook.jsx
@@ -1,9 +1,54 @@
 import '../App.css'
+import { useState } from 'react'
 
-function AddBook() {
-    return (
+const AddBook = () => {
+    const [newBook, setNewBook ] = useState({
+        name:"",
+        pages:"",
+        image:"",
+    })
+
+    const handleAddBook = () => {
+        console.log(newBook)
+    }
+
+    return(
         <>
-            <h1>This is the add book page</h1>
+            <header>
+                <h1>This is the add book page</h1>
+            </header>
+            <main>
+                <label>Please add your book name:</label><br/>
+                <input
+                    placeholder='Book Name' 
+                    name='name' 
+                    value={newBook.name}
+                    onChange={(e) => setNewBook({ ...newBook, name: e.target.value})} 
+                />
+                <br/><br/>
+                <label>Number of pages:</label><br/>
+                <input
+                    placeholder='num. pages' 
+                    name='pages'
+                    type='number'
+                    value={newBook.pages}
+                    onChange={(e) => setNewBook({ ...newBook, pages: e.target.value})} 
+                />
+                <br/><br/>
+                <label>Image link:</label><br/>
+                <input
+                    placeholder='link' 
+                    name='image' 
+                    value={newBook.image}
+                    onChange={(e) => setNewBook({ ...newBook, image: e.target.value})} 
+                />
+                <br/><br/>
+                <button
+                    onClick={handleAddBook}
+                >
+                    Submit
+                </button>
+            </main>
         </>
     )
 }
