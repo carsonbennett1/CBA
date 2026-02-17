cdaa52efe734ca84e173d678b1e44f1fe7024da6
Carson Bennett <carben2004@gmail.com>
2026-02-16 16:11:55 -0800

    working on buttons and useState

--- frontend/src/pages/Demo.jsx
+++ frontend/src/pages/Demo.jsx
@@ -1,11 +1,31 @@
 import { useState } from "react";
 import Header from "../components/header/Header";
+import "../styles/Demo.css"
 
 function Demo() {
+
+    const [random, setRandom] = useState(0);
+
+    function getRandom(){
+        let random = Math.floor(Math.random() * 256)
+        setRandom(random)
+    }
 
     return(
         <>
             <Header />
+            <div id="main">
+                <h3>Difficulty Selector:</h3>
+
+                <button className="button-spacing">EASY</button>
+                <button className="button-spacing">HARD</button>
+
+                <h3>Click Button Below to Generate Random Number</h3>
+                <button onClick={getRandom}>Random Button</button>
+                <p className="output">
+                    {random}
+                </p>
+            </div>
         </>
     )
 
