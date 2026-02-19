16846abd5afbd2533b610cd8a9655f9d5640445a
Carson Bennett <carben2004@gmail.com>
2026-02-18 21:04:06 -0800

    finished vanilla frontend for register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,52 @@
+import "../styles/Register.css"
+
 function Register() {
 
     return(
         <>
+            <h1>Register Page</h1>
+            <form>
+                <div className="form">
+                    <label htmlFor="email">
+                        <strong>Name:</strong>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Name"
+                        autoComplete="off"
+                        name="user-name"
+                        className="input-info"
+                    />
+                </div>
+                <div className="form">
+                    <label htmlFor="email">
+                        <strong>Email:</strong>
+                    </label>
+                    <input
+                        type="email"
+                        placeholder="Enter email"
+                        name="email"
+                        className="input-info"
+                    />
+                </div>
+                <div className="form">
+                    <label htmlFor="email">
+                        <strong>Password:</strong>
+                    </label>
+                    <input
+                        type="password"
+                        placeholder="Enter Password"
+                        name="password"
+                        className="input-info"
+                    />
+                </div>
+                <br></br>
+                <button className="signup-button">
+                    Register
+                </button>
+            </form>
         </>
     )
-
 }
 
 export default Register;
